3736a587e4d85b0381655921c170ad92ebec585e
Tom Rousseaux <rousseaux1.linerider@gmail.com>
2018-02-25 15:12:29 +0100

    learning to use this API

--- src/server.c
+++ src/server.c
@@ -1835,7 +1835,7 @@ static int server_main (server * const srv, int argc, char **argv) {
 				for (i = 0; i < conns->size; i++) {
 					connection *con = conns->ptr[i];
 
-					int optlen = MAX_SUBFLOWS*sizeof(struct mptcp_sub_status);
+					socklen_t optlen = MAX_SUBFLOWS*sizeof(struct mptcp_sub_status);
   					ids = (struct mptcp_sub_ids *) malloc(optlen);
   					if(ids==NULL) {
 						log_error_write(srv, __FILE__, __LINE__, "s",
